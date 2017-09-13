342b3b5c126ff0619b0b105436f35cda3614b094
Roe <croe@uk179495.uk.deloitte.com>
2017-09-13 19:14:05 +0100

    added tree data

---
+++ TestOrgDataWithRelationships.js
@@ -0,0 +1,243 @@
+var orgData = {
+  "name": "Organisation",
+  "children": [{
+      "name": "Deloitte Ventures",
+      "children": [{
+        "name": "Deloitte Consulting Incubator",
+        "relationships": [{
+          "target": "P11",
+          "value": "13"
+        }, {
+          "target": "P8",
+          "value": "3"
+        }, {
+          "target": "P4",
+          "value": "2"
+        }, {
+          "target": "P2",
+          "value": "3"
+        }, {
+          "target": "P9",
+          "value": "3"
+        }, {
+          "target": "P1",
+          "value": "3"
+        }, {
+          "target": "P20",
+          "value": "3"
+        }]
+      }, {
+        "name": "CTO Function",
+        "relationships": [{
+          "target": "P19",
+          "value": "4"
+        }, {
+          "target": "P18",
+          "value": "3"
+        }, {
+          "target": "P15",
+          "value": "3"
+        }, {
+          "target": "P14",
+          "value": "3"
+        }, {
+          "target": "P13",
+          "value": "3"
+        }, {
+          "target": "P5",
+          "value": "7"
+        }]
+      }, {
+        "name": "External Ventures",
+        "relationships": [{
+          "target": "P1",
+          "value": "3"
+        }, {
+          "target": "P13",
+          "value": "3"
+        }, {
+          "target": "P2",
+          "value": "3"
+        }, {
+          "target": "P4",
+          "value": "3"
+        }, {
+          "target": "P6",
+          "value": "12"
+        }, {
+          "target": "P14",
+          "value": "6"
+        }]
+      }, {
+        "name": "Product Factory",
+        "relationships": [{
+          "target": "P16",
+          "value": "14"
+        }, {
+          "target": "P19",
+          "value": "3"
+        }, {
+          "target": "P11",
+          "value": "4"
+        }]
+      }]
+    }, {
+      "name": "Human Capital",
+      "children": [{
+        "name": "HR",
+        "children": [{
+          "name": "PWA",
+          "relationships": [{
+            "target": "P10",
+            "value": "13"
+          }, {
+            "target": "P11",
+            "value": "3"
+          }, {
+            "target": "P7",
+            "value": "3"
+          }, {
+            "target": "P3",
+            "value": "3"
+          }, {
+            "target": "P4",
+            "value": "2"
+          }]
+        }, {
+          "name": "HRT",
+          "relationships": [{
+            "target": "P1",
+            "value": "6"
+          }, {
+            "target": "P3",
+            "value": "11"
+          }, {
+            "target": "P8",
+            "value": "2"
+          }, {
+            "target": "P13",
+            "value": "5"
+          }, {
+            "target": "P4",
+            "value": "2"
+          }]
+        }]
+      }, {
+        "name": "OT and T",
+        "relationships": [{
+          "target": "P11",
+          "value": "13"
+        }, {
+          "target": "P15",
+          "value": "3"
+        }, {
+          "target": "P13",
+          "value": "3"
+        }, {
+          "target": "P1",
+          "value": "3"
+        }, {
+          "target": "P9",
+          "value": "3"
+        }, {
+          "target": "P7",
+          "value": "3"
+        }, {
+          "target": "P19",
+          "value": "3"
+        }, {
+          "target": "P17",
+          "value": "2"
+        }]
+      }, {
+        "name": "ARA",
+        "relationships": [{
+          "target": "P16",
+          "value": "13"
+        }, {
+          "target": "P5",
+          "value": "3"
+        }, {
+          "target": "P15",
+          "value": "2"
+        }]
+      }]
+    },
+    {
+      "name": "Strategy and Operations",
+      "children": [{
+        "name": "Monitor Deloitte",
+        "children": [{
+          "name": "GrowthPath",
+          "relationships": [{
+            "target": "P2",
+            "value": "13"
+          }, {
+            "target": "P5",
+            "value": "3"
+          }, {
+            "target": "P4",
+            "value": "3"
+          }, {
+            "target": "P17",
+            "value": "3"
+          }, {
+            "target": "P11",
+            "value": "3"
+          }, {
+            "target": "P14",
+            "value": "2"
+          }]
+        }]
+      }, {
+        "name": "Doblin",
+        "relationships": [{
+          "target": "P1",
+          "value": "2"
+        }, {
+          "target": "P3",
+          "value": "3"
+        }, {
+          "target": "P7",
+          "value": "14"
+        }, {
+          "target": "P8",
+          "value": "8"
+        }, {
+          "target": "P12",
+          "value": "17"
+        }, {
+          "target": "P11",
+          "value": "20"
+        }, {
+          "target": "P20",
+          "value": "8"
+        }, {
+          "target": "P4",
+          "value": "10"
+        }]
+      }, {
+        "name": "BMT",
+        "relationships": [{
+          "target": "P20",
+          "value": "13"
+        }, {
+          "target": "P12",
+          "value": "3"
+        }, {
+          "target": "P15",
+          "value": "3"
+        }, {
+          "target": "P3",
+          "value": "3"
+        }, {
+          "target": "P7",
+          "value": "3"
+        }, {
+          "target": "P17",
+          "value": "2"
+        }]
+      }]
+    }
+  ]
+}
